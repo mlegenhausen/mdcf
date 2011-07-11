a2497218874eb42c2226503db3ed8b7e9277534f
mlegenhausen <mlegenhausen@gmail.com>
2011-07-11 17:15:32 +0200

    MdcfRemoteModule added for easier use with guice.

--- mdcf-locationtracker-remote/src/main/java/de/uniluebeck/itm/mdcf/remote/locationtracker/server/LocationTrackerServletModule.java
+++ mdcf-locationtracker-remote/src/main/java/de/uniluebeck/itm/mdcf/remote/locationtracker/server/LocationTrackerServletModule.java
@@ -1,21 +1,13 @@
 package de.uniluebeck.itm.mdcf.remote.locationtracker.server;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
 import com.google.inject.persist.PersistFilter;
 import com.google.inject.persist.jpa.JpaPersistModule;
 import com.google.inject.servlet.ServletModule;
 import com.trycatchsoft.gwt.requestfactory.InjectedRequestFactoryModule;
 import com.trycatchsoft.gwt.requestfactory.InjectedRequestFactoryServlet;
 
+import de.uniluebeck.itm.mdcf.remote.MdcfRemoteModule;
 import de.uniluebeck.itm.mdcf.remote.TransferRequestReceiver;
-import de.uniluebeck.itm.mdcf.remote.TransferRequestProcessor;
-import de.uniluebeck.itm.mdcf.remote.gson.ItemDeserializer;
-import de.uniluebeck.itm.mdcf.remote.gson.ValueDeserializer;
-import de.uniluebeck.itm.mdcf.remote.model.Item;
-import de.uniluebeck.itm.mdcf.remote.model.Value;
 
 public class LocationTrackerServletModule extends ServletModule {
 
@@ -23,21 +15,11 @@ public class LocationTrackerServletModule extends ServletModule {
 	protected void configureServlets() {
 		install(new JpaPersistModule("locationTrackerPersistenceUnit"));
 		install(new InjectedRequestFactoryModule());
-		
-		bind(TransferRequestProcessor.class).to(TransferRequestProcessorImpl.class);
+		install(new MdcfRemoteModule(TransferRequestProcessorImpl.class));
 		
 		filter("/*").through(PersistFilter.class);
 		
 		serve("/receiver").with(TransferRequestReceiver.class);
 		serve("/gwtRequest").with(InjectedRequestFactoryServlet.class);
-	}
-	
-	@Provides
-	@Singleton
-	public Gson provideGson(ItemDeserializer itemDeserializer, ValueDeserializer valueDeserializer) {
-		return new GsonBuilder()
-		.registerTypeAdapter(Item.class, itemDeserializer)
-		.registerTypeAdapter(Value.class, valueDeserializer)
-		.create();
 	}
 }
